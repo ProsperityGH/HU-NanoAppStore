d8a8f7517b8c738527a582538bac3589baaf22e7
ProsperityGH <90307071+ProsperityGH@users.noreply.github.com>
2024-09-14 14:45:51 +0200

    hangman

---
+++ hangman.py
@@ -0,0 +1,45 @@
+import random
+
+def hangman(word):
+    wrong = 0
+    stages = ["",
+                "__________       ",
+                "|        |      ",
+                "|        |      ",
+                "|        0      ",
+                "|       /|\     ",
+                "|       / \     ",
+                "|               "
+                ]
+    rletters = list(word)
+    board = ["__"] * len(word)
+    win = False
+    print("Welcome to Hangman")
+
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "Guess a letter: "
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+        else:
+            wrong += 1
+
+        print((" ".join(board)))
+        e = wrong + 1
+        print("\n".join(stages[0: e]))
+        
+        if "__" not in board:
+            print("You win!")
+            print(" ".join(board))
+            win = True
+            break
+    if not win:
+        print("\n".join(stages[0: wrong]))
+        print("You lose! It was {}.".format(word))
+
+words = ["cat", "dog", "bird", "fish", "elephant"]
+word = random.choice(words)
+hangman(word)
